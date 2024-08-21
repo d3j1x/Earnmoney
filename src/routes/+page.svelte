4020d0d40947898f36e3b817d7d51ec578a3558d
d3j1x <d3j1x@yahoo.com>
2024-08-22 00:45:36 +0200

    update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,10 @@
 
     import pawnslogo from "$lib/img/pwnslg.png";
     import screenshootph from "$lib/img/pxel.png";
-    import ads from "$lib/img/pxel3.png";
+    import ads from "$lib/img/pxel6.png";
     import honeygainlogo from "$lib/img/honeygainlg.png";
     import coinpayulogo from "$lib/img/coinpayulg.png";
+    import pilogo from "$lib/img/pi.png";
     import adbtclogo from "$lib/img/adbtclg.png"
     import { Indicator, Card, Badge} from 'flowbite-svelte';
     import { GiftBoxSolid} from 'flowbite-svelte-icons';
@@ -25,26 +26,26 @@
 
 </div>
 
-<div style="background-image: url({screenshootph});" class="bg-center bg-no-repeat bg-cover md:bg-fit pb-2">
+<div style="background-image: url({screenshootph});" class="bg-center bg-no-repeat bg-cover md:bg-fit pb-5">
 
   <div class="p-4 text-center">
     <h1>
       <Indicator color="green" class="animate-pulse md:ml-10 lg:ml-32 xl:ml-60"/>
-      <span class="font-extrabold text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-green-400  rounded px-2">
+      <span class="font-extrabold text-2xl md:text-3xl lg:text-4xl xl:text-6xl text-green-400  rounded px-2">
         BEST Free Automatic Profitable Apps
       </span>
     </h1>
   </div>
 
-  <div class="grid grid-cols-1 md:grid-cols-2 gap-10 m-5">
+  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 md:gap-2 m-5 xl:my-32">
     <div class="col-span-1 mx-auto md:my-20">
         <Card class="bg-transparent backdrop-blur-sm shadow-purple-500 border-none hover:backdrop-blur-none ">
             <img class="w-10 h-10 rounded-full" src={pawnslogo} alt="Karen profile" />
             <a href="https://pawns.app/?r=1255808" target="_blank" rel="noreferrer">
-              <h5 class="my-2 text-center text-xl md:text-2xl lg:text-3xl tracking-tight text-purple-700">
+              <h5 class="my-2 text-center text-2xl md:text-2xl lg:text-3xl tracking-tight text-purple-700">
                 Pawns App</h5>
             </a>
-            <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 lg:text-lg ">If you care for your time, Pawns.app lets you earn money instantly.</p>
+            <p class="mb-3 font-normal text-gray-500 dark:text-green-400 lg:text-lg ">If you care for your time, Pawns.app lets you earn money instantly.</p>
             <h1 style="font-family: 'Courier New', Courier, monospace;" class="text-primary-100">Mobile | Desktop</h1>
             <a href="https://pawns.app/?r=1255808" target="_blank" rel="noreferrer" class="inline-flex items-center text-purple-600 hover:underline font-semibold">
               Join now
@@ -56,7 +57,7 @@
         <Card class="bg-transparent backdrop-blur-sm shadow-amber-500 border-none hover:backdrop-blur-none">
             <img class="w-10 h-10 rounded-full" src={honeygainlogo} alt="Karen profile" />
             <a href="https://r.honeygain.me/CRYPTDED49" target="_blank" rel="noreferrer">
-              <h5 class="my-2 text-xl md:text-2xl lg:text-3xl  tracking-tight text-center text-amber-500 ">
+              <h5 class="my-2 text-2xl md:text-2xl lg:text-3xl  tracking-tight text-center text-amber-500 ">
                 Honeygain App</h5>
             </a>
             <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 lg:text-lg">Download the Honeygain app today and make money effortlessly.
@@ -69,34 +70,52 @@
         </Card>
     </div>
     
+    <div class="col-span-1 md:col-span-2 xl:col-span-1 m-auto">
+      <Card class="bg-transparent backdrop-blur-sm shadow-primary-300 border-none hover:backdrop-blur-none">
+          <img class="w-10 h-10 rounded-full" src={pilogo} alt="Karen profile" />
+          <a href="https://minepi.com/d3j1x" target="_blank" rel="noreferrer">
+            <h5 class="my-2 text-2xl md:text-2xl lg:text-3xl  tracking-tight text-center text-primary-300 ">
+              Pi App</h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 lg:text-lg">
+            The first social crypto that you can mine on your mobile phone.
+          </p>
+          <h1 style="font-family: 'Courier New', Courier, monospace;" class="text-primary-100">Mobile | Desktop</h1>
+          <a href="https://minepi.com/d3j1x" target="_blank" rel="noreferrer" class="inline-flex items-center text-primary-300 hover:underline font-semibold">
+            Join now
+            <GiftBoxSolid class="w-4 h-4 ms-2.5" />
+          </a>
+      </Card>
+  </div>
+
   </div>
 
 
 <h1 class="text-center">
-  <Badge large color="green" class="text-center ">Level 1</Badge>
+  <Badge large color="green" class="text-center my-2 ">Level 1</Badge>
 </h1>
 
 </div>
 
 
-<div style="background-image: url({ads});" class="bg-center bg-no-repeat bg-cover md:bg-fit pb-2">
+<div style="background-image: url({ads});" class="bg-center bg-no-repeat bg-cover md:bg-fit pb-5">
 
 <div class="p-4 text-center">
   <h1 class=" ">
-    <Indicator color="blue" class="animate-pulse md:ml-10 lg:ml-32 xl:ml-60"/>
-  <span class="font-extrabold text-2xl md:text-3xl lg:text-4xl xl:text-5xl  text-blue-700">
+    <Indicator  class="animate-pulse md:ml-10 lg:ml-32 xl:ml-60"/>
+  <span class="font-extrabold text-2xl md:text-3xl lg:text-4xl xl:text-6xl  text-white">
     Earn money by watching Ads</span></h1>
 </div>
 
 
-<div class="grid grid-cols-1 md:grid-cols-2 gap-10 my-5">
+<div class="grid grid-cols-1 md:grid-cols-2 gap-10 m-5 xl:my-32">
   <div class="col-span-1 mx-auto md:my-20">
       <Card class="bg-transparent backdrop-blur-sm shadow-blue-500 border-none hover:backdrop-blur-none ">
           <img class="w-10 h-10 rounded-full" src={coinpayulogo} alt="Karen profile" />
           <a href="https://www.coinpayu.com/?r=d3j1x" target="_blank" rel="noreferrer">
-            <h5 class="my-2 text-xl md:text-2xl lg:text-3xl  tracking-tight text-center text-blue-700">Coinpayu</h5>
+            <h5 class="my-2 text-2xl md:text-2xl lg:text-3xl  tracking-tight text-center text-blue-600">Coinpayu</h5>
           </a>
-          <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Coinpayu is Platform where you can earn crypto by viewing advertisements or doing offers.</p>
+          <p class="mb-3 font-normal text-black dark:text-gray-400">Coinpayu is Platform where you can earn crypto by viewing advertisements or doing offers.</p>
           <h1 style="font-family: 'Courier New', Courier, monospace;" class="text-primary-100">Mobile | Desktop</h1>
           <a href="https://www.coinpayu.com/?r=d3j1x" target="_blank" rel="noreferrer" class="inline-flex items-center text-blue-600 hover:underline font-semibold">
             Join now
@@ -108,12 +127,12 @@
       <Card class="bg-transparent backdrop-blur-sm shadow-gray-500 border-none hover:backdrop-blur-none ">
           <img class="w-10 h-10 rounded-full" src={adbtclogo} alt="Karen profile" />
           <a href="https://r.adbtc.top/3118297" target="_blank" rel="noreferrer">
-            <h5 class="my-2 text-xl md:text-2xl lg:text-3xl  tracking-tight text-center text-gray-700">Adbtc</h5>
+            <h5 class="my-2 text-2xl md:text-2xl lg:text-3xl  tracking-tight text-center text-gray-200">Adbtc</h5>
           </a>
-          <p class="mb-3 font-normal text-gray-500 ">Earn bitcoin online by viewing websites. Cheap website promotion with bitcoin payments.
+          <p class="mb-3 font-normal text-black ">Earn bitcoin online by viewing websites. Cheap website promotion with bitcoin payments.
           </p>
           <h1 style="font-family: 'Courier New', Courier, monospace;" class="text-primary-100">Mobile | Desktop</h1>
-          <a href="https://r.adbtc.top/3118297" target="_blank" rel="noreferrer" class="inline-flex items-center text-gray-700 hover:underline font-semibold">
+          <a href="https://r.adbtc.top/3118297" target="_blank" rel="noreferrer" class="inline-flex items-center text-gray-200 hover:underline font-semibold">
             Join now
             <GiftBoxSolid class="w-4 h-4 ms-2.5" />
           </a>
@@ -122,7 +141,7 @@
 </div>
 
 <h1 class="text-center">
-  <Badge large color="indigo" class="text-center">Level 2</Badge>
+  <Badge large color="indigo" class="text-center my-2">Level 2</Badge>
 </h1>
 </div>
 
