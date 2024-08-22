cdb60739b78185f6ea64909f7f08c7009d4387a2
d3j1x <d3j1x@yahoo.com>
2024-08-22 16:50:28 +0200

    Added Level 3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,15 @@
     import pawnslogo from "$lib/img/pwnslg.png";
     import screenshootph from "$lib/img/pxel.png";
     import ads from "$lib/img/pxel6.png";
+    import tfaucet from "$lib/img/hello.png";
     import honeygainlogo from "$lib/img/honeygainlg.png";
     import coinpayulogo from "$lib/img/coinpayulg.png";
     import pilogo from "$lib/img/pi.png";
     import fplogo from "$lib/img/fp2.png";
-    import adbtclogo from "$lib/img/adbtclg.png"
+    import adbtclogo from "$lib/img/adbtclg.png";
+    import earnbtclogo from "$lib/img/earnbtc.png";
+    import faucetcrypto from "$lib/img/fc.png";
+    import simpleb from "$lib/img/simpleb.png";
     import { Indicator, Card, Badge, Alert } from 'flowbite-svelte';
     import { GiftBoxSolid,  InfoCircleSolid} from 'flowbite-svelte-icons';
 
@@ -38,7 +42,7 @@
 
 </div>
 
-<div style="background-image: url({screenshootph});" class="bg-center bg-no-repeat bg-cover md:bg-fit pb-5">
+<div style="background-image: url({screenshootph});" class="bg-center bg-no-repeat bg-cover pb-5">
 
   <div class="px-4 py-10 text-center">
     <h1>
@@ -113,42 +117,42 @@
 </div>
 
 
-<div style="background-image: url({ads});" class="bg-center bg-no-repeat bg-cover md:bg-fit pb-5">
+<div style="background-image: url({ads});" class="bg-center bg-no-repeat bg-cover pb-5">
 
 <div class="px-4 py-10 text-center">
   <h1 class=" ">
-    <Indicator color="blue"  class="animate-pulse md:ml-10 lg:ml-32 xl:ml-60"/>
-  <span class="font-extrabold text-2xl md:text-3xl lg:text-4xl xl:text-6xl  text-blue-600">
-    Earn money by watching Ads</span></h1>
+    <Indicator  class="animate-pulse md:ml-10 lg:ml-32 xl:ml-60"/>
+  <span class="font-extrabold text-2xl md:text-3xl lg:text-4xl xl:text-6xl  text-primary-50">
+    Earn Money by Watching Ads , Offers</span></h1>
 </div>
 
 
 <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 m-5 xl:my-24">
   <div class="col-span-1 mx-auto md:my-20">
-      <Card class="bg-transparent hover:backdrop-blur-sm shadow-blue-500 border-none backdrop-blur-none ">
+      <Card class="bg-transparent hover:backdrop-blur-sm shadow-primary-50 border-none backdrop-blur-none ">
           <img class="w-10 h-10 rounded-full" src={coinpayulogo} alt="Karen profile" />
           <a href="https://www.coinpayu.com/?r=d3j1x" target="_blank" rel="noreferrer">
-            <h5 class="my-2 text-2xl md:text-2xl lg:text-3xl  tracking-tight text-center text-blue-600">Coinpayu</h5>
+            <h5 class="my-2 text-2xl md:text-2xl lg:text-3xl  tracking-tight text-center text-primary-50">Coinpayu</h5>
           </a>
           <p class="mb-3 font-bold text-gray-700 lg:text-lg">
             Coinpayu is Platform where you can earn crypto by viewing advertisements or doing offers.</p>
           <h1 style="font-family: 'Courier New', Courier, monospace;" class="text-primary-100">Mobile | Desktop</h1>
-          <a href="https://www.coinpayu.com/?r=d3j1x" target="_blank" rel="noreferrer" class="inline-flex items-center text-blue-600 hover:underline font-semibold">
+          <a href="https://www.coinpayu.com/?r=d3j1x" target="_blank" rel="noreferrer" class="inline-flex items-center text-primary-50 hover:underline font-semibold">
             Join now
             <GiftBoxSolid class="w-4 h-4 ms-2.5" />
           </a>
       </Card>
   </div>
   <div class="col-span-1 m-auto">
-      <Card class="bg-transparent hover:backdrop-blur-sm shadow-primary-100 border-none backdrop-blur-none ">
+      <Card class="bg-transparent hover:backdrop-blur-sm shadow-primary-50 border-none backdrop-blur-none ">
           <img class="w-10 h-10 rounded-full" src={adbtclogo} alt="Karen profile" />
           <a href="https://r.adbtc.top/3118297" target="_blank" rel="noreferrer">
-            <h5 class="my-2 text-2xl md:text-2xl lg:text-3xl  tracking-tight text-center text-primary-100">Adbtc</h5>
+            <h5 class="my-2 text-2xl md:text-2xl lg:text-3xl  tracking-tight text-center text-primary-50">Adbtc</h5>
           </a>
           <p class="mb-3 font-bold text-gray-700 lg:text-lg">Earn bitcoin online by viewing websites. Cheap website promotion with bitcoin payments.
           </p>
           <h1 style="font-family: 'Courier New', Courier, monospace;" class="text-primary-100">Mobile | Desktop</h1>
-          <a href="https://r.adbtc.top/3118297" target="_blank" rel="noreferrer" class="inline-flex items-center text-primary-100 hover:underline font-semibold">
+          <a href="https://r.adbtc.top/3118297" target="_blank" rel="noreferrer" class="inline-flex items-center text-primary-50 hover:underline font-semibold">
             Join now
             <GiftBoxSolid class="w-4 h-4 ms-2.5" />
           </a>
@@ -156,17 +160,17 @@
   </div>
 
   <div class="col-span-1 md:col-span-2 xl:col-span-1 m-auto">
-    <Card class="bg-transparent hover:backdrop-blur-sm shadow-black border-none backdrop-blur-none">
+    <Card class="bg-transparent hover:backdrop-blur-sm shadow-primary-50 border-none backdrop-blur-none">
         <img class="w-10 h-10 rounded-full" src={fplogo} alt="Karen profile" />
         <a href="https://faucetpay.io/?r=4173092" target="_blank" rel="noreferrer">
-          <h5 class="my-2 text-2xl md:text-2xl lg:text-3xl  tracking-tight text-center text-black ">
+          <h5 class="my-2 text-2xl md:text-2xl lg:text-3xl  tracking-tight text-center text-primary-50 ">
             Faucetpay</h5>
         </a>
         <p class="mb-3 font-bold text-gray-700 lg:text-lg dark:text-gray-400 ">
           Micropayment Wallet & Earnings Platform. Ads & offres.
         </p>
         <h1 style="font-family: 'Courier New', Courier, monospace;" class="text-primary-100">Mobile | Desktop</h1>
-        <a href="https://faucetpay.io/?r=4173092" target="_blank" rel="noreferrer" class="inline-flex items-center text-black hover:underline font-semibold">
+        <a href="https://faucetpay.io/?r=4173092" target="_blank" rel="noreferrer" class="inline-flex items-center text-primary-50 hover:underline font-semibold">
           Join now
           <GiftBoxSolid class="w-4 h-4 ms-2.5" />
         </a>
@@ -177,7 +181,7 @@
 </div>
 
 <h1 class="text-center">
-  <Badge large color="blue" class="text-center my-2">Level 2</Badge>
+  <Badge large color="dark" class="text-center my-2">Level 2</Badge>
 </h1>
 </div>
 
@@ -188,10 +192,87 @@
 
 
 
-<Alert color="blue" class="rounded-none h-10">
+<div style="background-image: url({tfaucet});" class="bg-center bg-no-repeat bg-cover pb-5">
+
+  <div class="px-4 py-10 text-center">
+    <h1 class=" ">
+      <Indicator color="red" class="animate-pulse md:ml-10 lg:ml-32 xl:ml-60"/>
+    <span class="font-extrabold text-2xl md:text-3xl lg:text-4xl xl:text-6xl  text-primary-900">
+      Best Trusted Crypto Faucets</span></h1>
+  </div>
+  
+  
+  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 m-5 xl:my-24">
+    <div class="col-span-1 mx-auto md:my-20">
+        <Card class="bg-transparent hover:backdrop-blur-sm shadow-primary-900 border-none backdrop-blur-none ">
+            <img class="w-10 h-10 rounded-full" src={earnbtclogo} alt="Karen profile" />
+            <a href="https://www.earnbitcoin.net/?r=468241" target="_blank" rel="noreferrer">
+              <h5 class="my-2 text-2xl md:text-2xl lg:text-3xl  tracking-tight text-center text-primary-900">EarnBitoin Net</h5>
+            </a>
+            <p class="mb-3 font-bold text-gray-700 lg:text-lg">
+              EarnBitcoin.net is the largest educational Bitcoin Faucet online! With 500,000+ happy users.</p>
+            <h1 style="font-family: 'Courier New', Courier, monospace;" class="text-primary-900">Mobile | Desktop</h1>
+            <a href="https://www.earnbitcoin.net/?r=468241" target="_blank" rel="noreferrer" class="inline-flex items-center text-primary-900 hover:underline font-semibold">
+              Join now
+              <GiftBoxSolid class="w-4 h-4 ms-2.5" />
+            </a>
+        </Card>
+    </div>
+    <div class="col-span-1 m-auto">
+        <Card class="bg-transparent hover:backdrop-blur-sm shadow-primary-900 border-none backdrop-blur-none ">
+            <img class="w-10 h-10 rounded-full" src={faucetcrypto} alt="Karen profile" />
+            <a href="https://r.adbtc.top/3118297" target="_blank" rel="noreferrer">
+              <h5 class="my-2 text-2xl md:text-2xl lg:text-3xl  tracking-tight text-center text-primary-900">Faucet Crypto</h5>
+            </a>
+            <p class="mb-3 font-bold text-gray-700 lg:text-lg">What are you waiting for? Grab your free coins today!</p>
+            <h1 style="font-family: 'Courier New', Courier, monospace;" class="text-primary-900">Mobile | Desktop</h1>
+            <a href="https://r.adbtc.top/3118297" target="_blank" rel="noreferrer" class="inline-flex items-center text-primary-900 hover:underline font-semibold">
+              Join now
+              <GiftBoxSolid class="w-4 h-4 ms-2.5" />
+            </a>
+        </Card>
+    </div>
+  
+    <div class="col-span-1 md:col-span-2 xl:col-span-1 m-auto">
+      <Card class="bg-transparent hover:backdrop-blur-sm shadow-primary-900 border-none backdrop-blur-none">
+          <img class="w-10 h-10 rounded-full" src={simpleb} alt="Karen profile" />
+          <a href="https://simplebits.io/ref/L4tmvQbH1IOWy5ro" target="_blank" rel="noreferrer">
+            <h5 class="my-2 text-2xl md:text-2xl lg:text-3xl  tracking-tight text-center text-primary-900 ">
+              Simplebits</h5>
+          </a>
+          <p class="mb-3 font-bold text-gray-700 lg:text-lg dark:text-gray-400 ">
+            Earn cryptocurrencies and get paid with our unique mining simulation game.
+          </p>
+          <h1 style="font-family: 'Courier New', Courier, monospace;" class="text-primary-900">Mobile | Desktop</h1>
+          <a href="https://simplebits.io/ref/L4tmvQbH1IOWy5ro" target="_blank" rel="noreferrer" class="inline-flex items-center text-primary-900 hover:underline font-semibold">
+            Join now
+            <GiftBoxSolid class="w-4 h-4 ms-2.5" />
+          </a>
+      </Card>
+  </div>
+  
+  
+  </div>
+  
+  <h1 class="text-center">
+    <Badge large color="red" class="text-center my-2">Level 3</Badge>
+  </h1>
+  </div>
+
+
+  
+  
+  <div class="bg-black">
+    <h1 class=" text-center font-mono text-xs text-primary-50">MΛKIПG MӨПΣY FЯӨM ƬΉΣ IПƬΣЯПΣƬ</h1>
+  </div>
+
+
+
+<Alert color="green" class="rounded-none h-10 ">
   <InfoCircleSolid slot="icon" class="w-5 h-5" />
   <span class="">Update alert!</span>
-  <span style="font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;" class="font-semibold">Level 3 Coming soon.</span>
+  <span style="font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;" class="font-semibold">
+    Level 3 added successfully.</span>
   
 </Alert>
 
